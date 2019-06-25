4484dcfa1b10317bdd643c790061172bbc1c55a4
Karrick S. McDermott <github@karrick.net>
2019-06-25 22:30:36 +0000

    remove wrapper function

--- asm_linux_amd64.s
+++ asm_linux_amd64.s
@@ -1,7 +1,7 @@
 #include "textflag.h"
 
-// func divmod(x1, x0, y Word) (q, r Word)
-TEXT ·divmod(SB),NOSPLIT,$0
+// func Divmod(x1, x0, y uint) (q, r uint)
+TEXT ·Divmod(SB),NOSPLIT,$0
 	MOVQ x1+0(FP), DX
 	MOVQ x0+8(FP), AX
 	DIVQ y+16(FP)
--- divmod.go
+++ divmod.go
@@ -1,10 +1,3 @@
 package divmod
 
-// A Word represents a single digit of a multi-precision unsigned integer.
-type Word uint
-
-func divmod(x1, x0, y uint) (q, r uint)
-
-func Divmod(x1, x0, y uint) (q, r uint) {
-	return divmod(x1, x0, y)
-}
+func Divmod(x1, x0, y uint) (q, r uint)
